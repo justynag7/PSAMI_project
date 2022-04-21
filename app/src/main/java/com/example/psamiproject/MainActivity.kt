0e8d4f5b1012fea85753036b6c40463a3b791355
Anna Foltyniewicz <foltyniewicz.anna@gmail.com>
2022-04-21 12:58:37 +0200

    Add Log in buttons

--- app/src/main/java/com/example/psamiproject/MainActivity.kt
+++ app/src/main/java/com/example/psamiproject/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.psamiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Zaloguj = findViewById(R.id.button_zaloguj) as Button
+        val Tekst = findViewById(R.id.tekst) as TextView
+        val Zarejestruj = findViewById(R.id.button_zarejestruj) as Button
+        Zaloguj.setOnClickListener() {
+            Tekst.setText("Zaloguj")
+        }
+        Zarejestruj.setOnClickListener(){
+            Tekst.setText("Zarejestruj")
+        }
+
     }
+
 }
